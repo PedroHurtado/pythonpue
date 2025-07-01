1be945cbf2be053610e737d145f148404500d177
Pedro Hurtado <pedrohurt@gmail.com>
2025-07-01 14:25:28 +0200

    entity base

---
+++ app/core/entitybase.py
@@ -0,0 +1,7 @@
+"""
+1. crear una clase abstracta
+2. llevar el id a la entity base(agregarlo al constructor y agregarlo property)
+3. reemplazar el __eq__ y el __hash__
+4. heredar en ingredeint y en pizza
+    super().__init(id)
+"""
--- app/dominio/pizza/pizza.py
+++ app/dominio/pizza/pizza.py
@@ -1,45 +1,55 @@
 import copy
 
+
 class Pizza:
-    def __init__(self,id,name,description,url,ingredients):
-        self._id= id,
+    PROFIT = 1.2
+
+    def __init__(self, id, name, description, url, ingredients):
+        self._id = (id,)
         self._name = name
         self._description = description
         self._url = url
         self._ingredients = copy.deepcopy(ingredients)
+
     @property
     def id(self):
         return self._id
+
     @property
     def name(self):
         return self._name
+
     @property
     def description(self):
         return self._description
+
     @property
     def url(self):
         return self._url
+
     @property
     def ingredient(self):
         return copy.deepcopy(self._ingredients)
+
     @property
     def price(self):
-        result = 0
-        for ingredient in self._ingredients:
-            result+=ingredient.cost
-        return result * 1.2            
-    def add_ingredient(self,ingredient):
-        #pizza.addingredient
+        return sum(cost for _, cost in self._ingredients) * Pizza.PROFIT
+
+    def add_ingredient(self, ingredient):
+        # pizza.addingredient
         self._ingredients.add(ingredient)
-    def remove_ingredient(self,ingredeint):
-        #pizza.removeingredient
+
+    def remove_ingredient(self, ingredeint):
+        # pizza.removeingredient
         self._ingredients.remove(ingredeint)
+
     @classmethod
-    def create(cls,id,name,description,url,ingredients):
-        #pizza.create
-        return cls(id,name,description,url,ingredients)
-    def update(self,name,description,url,ingredients):
-        #pizza.update
+    def create(cls, id, name, description, url, ingredients):
+        # pizza.create
+        return cls(id, name, description, url, ingredients)
+
+    def update(self, name, description, url, ingredients):
+        # pizza.update
         self._name = name
         self._description = description
         self._url = url
