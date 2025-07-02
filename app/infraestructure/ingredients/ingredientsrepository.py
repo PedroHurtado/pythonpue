4dc96dc19dd5b977063eb563ec953c8e02660856
Pedro Hurtado <pedrohurt@gmail.com>
2025-07-02 11:40:16 +0200

    singleton repository ingredient

--- app/infraestructure/ingredients/ingredientsrepository.py
+++ app/infraestructure/ingredients/ingredientsrepository.py
@@ -1,7 +1,17 @@
-from core.repository import Add,Update,Remove
+from core.repository import Add, Update, Remove
 from dominio.ingredient.ingredient import Ingredient
-class IngredientRepository(Add,Update,Remove):
-    def __init__(self, data:set[Ingredient]):
+
+
+class IngredientRepository(Add, Update, Remove):
+    def __init__(self, data: set[Ingredient]):
         super().__init__(data)
-    def find_by_name(self,name:str):
+    def query(predicate,page=0,size=10):
+        """
+        sobre el data aplicar un filter y obtener 
+        solo pagina indicada y el numero de elementos por página
+        """
         pass
+    
+ingredient_repository =IngredientRepository(set())
+
+__all__ = ['ingredient_repository']
