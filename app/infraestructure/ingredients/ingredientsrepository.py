85fdbbf38ef5d03efcc824d2b42ce69101504558
Pedro Hurtado <pedrohurt@gmail.com>
2025-07-02 14:51:55 +0200

    doc restfull

--- app/infraestructure/ingredients/ingredientsrepository.py
+++ app/infraestructure/ingredients/ingredientsrepository.py
@@ -5,13 +5,16 @@ from dominio.ingredient.ingredient import Ingredient
 class IngredientRepository(Add, Update, Remove):
     def __init__(self, data: set[Ingredient]):
         super().__init__(data)
-    def query(predicate,page=0,size=10):
-        """
-        sobre el data aplicar un filter y obtener 
-        solo pagina indicada y el numero de elementos por página
-        """
-        pass
-    
-ingredient_repository =IngredientRepository(set())
 
-__all__ = ['ingredient_repository']
+    def query(self, predicate, page=0, size=10):
+        filtered_data = [item for item in self.data if predicate(item)]
+
+        start_index = page * size
+        end_index = start_index + size
+
+        return filtered_data[start_index:end_index]
+
+
+ingredient_repository = IngredientRepository(set())
+
+__all__ = ["ingredient_repository"]
