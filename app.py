3ab517472ed55e1c4cd6d5024cfce49dd1e49290
Pedro Hurtado <pedrohurt@gmail.com>
2025-07-03 12:52:53 +0200

    response custom headers

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask,send_from_directory
+from flask import Flask,send_from_directory,jsonify
 
 app = Flask(__name__)
+
+
+
+@app.after_request
+def add_custom_header(response):
+    # Se ejecutará después de cada petición
+    response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    return response
 
 @app.route("/")
 def index():
@@ -8,7 +17,13 @@ def index():
 
 @app.route("/about")
 def about():
-    return "<h1>Acerca de nosotros<h1>"
+    data={
+        "id":1,
+        "name": "Pedro Hurtado"
+    }
+    return jsonify(data),201, {
+        "X-App": "pyton"
+    }
 
 if __name__ == '__main__':
     app.run(debug=True)
